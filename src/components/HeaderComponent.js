e87d15a52ad0e009d0ba53e2bc2a632547b9ee4c
Corey Benedict-Osborn <corey.benedict@gmail.com>
2022-03-17 20:24:49 -0400

    React Wk 3 - Adding to the Single Page App

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -41,7 +41,14 @@ class Header extends Component {
         </Jumbotron>
         <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="NuCamp Logo"
+              />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNav} />
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
@@ -62,7 +69,7 @@ class Header extends Component {
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="navlink" to="/home">
+                  <NavLink className="navlink" to="/contactus">
                     <i className="fa fa-address-card fa-lg" /> Contact Us
                   </NavLink>
                 </NavItem>
